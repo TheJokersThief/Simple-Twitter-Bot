f65c839f6412ce96abaf22335d6be5189d6fe677
Evan Smith <esincork@gmail.com>
2016-09-02 15:25:32 +0100

    Flag to disable tweets if the account tweeted recently

--- Tweet.py
+++ Tweet.py
@@ -5,6 +5,7 @@ import ConfigParser
 import twitter
 import time
 import datetime
+from dateutil import parser
 from random import randint
 
 import requests.packages.urllib3
@@ -26,62 +27,81 @@ class Tweet:
         self.my_access_token_key = self.settings['twitter_access_token_key']
         self.my_access_token_secret = self.settings['twitter_access_token_secret']
 
-
-    def tweet( self, title, url, include_quotes ):
-            # calculate the chars needed (without title)
-            # twitter shortens URLs at 22 (http) or 23 (https) chars
-            self.chars = 4  # 2 spaces + 2 braces
-            if ( url[0:8] == 'https://' ):
-                    if len( url ) < 23:
-                            self.chars += len( url )
-                    else:
-                            self.chars += 23
-            elif ( url[0:7] == 'http://' ):
-                    if len( url ) < 22:
-                            self.chars += len( url )
-                    else:
-                            self.chars += 22
-            elif ( url == '' ):  # it's a story on HN
-                    self.chars -= 3  # no space + braces for comment link
-            else:
-                    print "Unknown URL schema: {0}".format( url )
-                    sys.exit(1)
-
-            self.chars += 23  # comment url
-
-            # cut title if too long
-            if (self.chars + len(title) > 140):
-                    title = "{0} ...".format(title[0:140 - self.chars - 4])
-
-            if( include_quotes ):
-                    t = "\"{0}\" {1}".format(title, url)
-            else:
-                    #t = "{0} {1} ({2})".format(title, url, comment_url)
-                    t = "{0} {1}".format(title, url)
-
-            isImage = False
-            if( url[-4:] == ".png" or url[-4:] == ".jpg" ):
-                extension = url[-3:]
-                isImage = True
-            elif( url[-5:] == ".jpeg" ):
-                extension = "jpeg"
-                isImage = True
-
-            # Tweet it
+    def check_last_tweet( self ):
+        if( self.settings['disable_if_posted_recently'] ):
             try:
                 api = twitter.Api(consumer_key = self.my_consumer_key,
-                                  consumer_secret = self.my_consumer_secret,
-                                  access_token_key = self.my_access_token_key,
-                                  access_token_secret = self.my_access_token_secret)
+                                    consumer_secret = self.my_consumer_secret,
+                                    access_token_key = self.my_access_token_key,
+                                    access_token_secret = self.my_access_token_secret)
 
-                if( isImage ):
-                    api.PostMedia(title, url)
-                else:
-                    api.PostUpdate(t)
-                print "Twitter Post succeeeded: " + str( t )
+                timeline = api.GetUserTimeline('thejokersthief')
+
+                last_tweet_datetime = parser.parse( timeline[0].created_at )
+                return (datetime.datetime.now() - last_tweet_datetime.replace(tzinfo=None)
+                            ) > datetime.timedelta( hours = self.settings['disable_time_hours'] )
+                
             except Exception as e:
                 print "{0}".format(e)
                 print "Posting to Twitter failed!"
+        else:
+            return true
+
+    def tweet( self, title, url, include_quotes ):
+        # calculate the chars needed (without title)
+        # twitter shortens URLs at 22 (http) or 23 (https) chars
+        self.chars = 4  # 2 spaces + 2 braces
+        if ( url[0:8] == 'https://' ):
+                if len( url ) < 23:
+                        self.chars += len( url )
+                else:
+                        self.chars += 23
+        elif ( url[0:7] == 'http://' ):
+                if len( url ) < 22:
+                        self.chars += len( url )
+                else:
+                        self.chars += 22
+        elif ( url == '' ):  # it's a story on HN
+                self.chars -= 3  # no space + braces for comment link
+        else:
+                print "Unknown URL schema: {0}".format( url )
+                sys.exit(1)
+
+        self.chars += 23  # comment url
+
+        # cut title if too long
+        if (self.chars + len(title) > 140):
+                title = "{0} ...".format(title[0:140 - self.chars - 4])
+
+        if( include_quotes ):
+                t = "\"{0}\" {1}".format(title, url)
+        else:
+                #t = "{0} {1} ({2})".format(title, url, comment_url)
+                t = "{0} {1}".format(title, url)
+
+        isImage = False
+        if( url[-4:] == ".png" or url[-4:] == ".jpg" ):
+            extension = url[-3:]
+            isImage = True
+        elif( url[-5:] == ".jpeg" ):
+            extension = "jpeg"
+            isImage = True
+
+        # Tweet it
+        try:
+            api = twitter.Api(consumer_key = self.my_consumer_key,
+                              consumer_secret = self.my_consumer_secret,
+                              access_token_key = self.my_access_token_key,
+                              access_token_secret = self.my_access_token_secret)
+
+            if( isImage ):
+                api.PostMedia(title, url)
+            else:
+                api.PostUpdate(t)
+            print "Twitter Post succeeeded: " + str( t )
+        except Exception as e:
+            print "{0}".format(e)
+            print "Posting to Twitter failed!"
 
     def copyFollowers( self, screen_name ):
         try:
--- main.py
+++ main.py
@@ -4,16 +4,20 @@
 from HackerNews import HackerNews
 from Reddit import Reddit
 from RSS import RSS
+from Tweet import Tweet
 
 # CONSTANTS
 RETRIES = 10 			# Number of retries on URL failure
 WAIT_TIME = 20 * 60 	# Time to wait between posting (in seconds)
 
-news = HackerNews( WAIT_TIME )
-news.tweet_stories( 5 )
+tweet = Tweet();
 
-reddit = Reddit( WAIT_TIME )
-reddit.tweet_stories( 3 )
+if( tweet.check_last_tweet() ):
+	news = HackerNews( WAIT_TIME )
+	news.tweet_stories( 5 )
 
-rss = RSS( WAIT_TIME )
-rss.tweet_stories( 2 )
+	reddit = Reddit( WAIT_TIME )
+	reddit.tweet_stories( 3 )
+
+	rss = RSS( WAIT_TIME )
+	rss.tweet_stories( 2 )
